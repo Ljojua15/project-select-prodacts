c305d4201ec14d436ad0a02f3d09c04857e36dd5
Ljojua15 <jojualasha15@gmail.com>
2024-10-09 16:43:13 +0400

    'flat-cards'

---
+++ src/app/interfaces/locations.ts
@@ -0,0 +1,14 @@
+export interface Location {
+  id: number;
+  name: string;
+  city: string;
+  state: string;
+  photo: string;
+  availableUnits: number;
+  wifi: boolean;
+  laundry: boolean;
+}
+
+export interface LocationsResponse {
+  locations: Location[];
+}
